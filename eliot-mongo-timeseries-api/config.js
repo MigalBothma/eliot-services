2694b1ca5a7ab8fb0490d132e633a02bbe0703e2
MigalBothma <42865700+MigalBothma@users.noreply.github.com>
2019-08-17 17:38:46 +0200

    Update config.js

--- eliot-mongo-timeseries-api/config.js
+++ eliot-mongo-timeseries-api/config.js
@@ -6,7 +6,7 @@ config.mongodb = {};
 config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'localhost';
 config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
 config.mongodb.database   = process.env.MONGODB_DATABASE   || 'mqtt';
-config.mongodb.collection = process.env.MONGODB_COLLECTION || 'messages';
+config.mongodb.collection = process.env.MONGODB_COLLECTION || 'events';
 
 config.app = {};
 config.app.port = process.env.PORT || 3000;
