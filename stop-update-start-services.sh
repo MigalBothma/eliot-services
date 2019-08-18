508ab3507968babb6022096b213518838c236026
Migal Bothma <migal.bothma@bcx.co.za>
2019-08-18 12:15:36 +0200

    prettification

--- stop-update-start-services.sh
+++ stop-update-start-services.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 echo "ELIoT Services Script"
+echo "---------------------------------------------------------------------------"
 
 echo "Changing directory to /usr/src/"
-cd /usr/src/
+echo "---------------------------------------------------------------------------"
+    cd /usr/src/
+echo "---------------------------------------------------------------------------"
 
 echo "Forever stop all services"
-forever stopall
+echo "---------------------------------------------------------------------------"
+    forever stopall
+echo "---------------------------------------------------------------------------"    
 
 echo "Pulling ELIoT Services from github"
-git stash
-git pull https://github.com/MigalBothma/eliot-services.git
+echo "---------------------------------------------------------------------------"
+    git stash
+    git pull https://github.com/MigalBothma/eliot-services.git
+echo "---------------------------------------------------------------------------"
 
 echo "NPM Install Service Dependencies"
-cd /usr/src/eliot-services/eliot-mqtt-mongodb/
-sudo npm install
-cd /usr/src/eliot-services/eliot-mongo-timeseries-api/
-sudo npm install
+echo "---------------------------------------------------------------------------"
+    cd /usr/src/eliot-services/eliot-mqtt-mongodb/
+    sudo npm install
+    cd /usr/src/eliot-services/eliot-mongo-timeseries-api/
+    sudo npm install
+echo "---------------------------------------------------------------------------"
 
 echo "Start ELIoT Services"
-forever start /usr/src/eliot-services/eliot-mqtt-mongodb/server.js
+echo "---------------------------------------------------------------------------"
+    forever start /usr/src/eliot-services/eliot-mqtt-mongodb/server.js
 echo "eliot-mqtt-mongodb Started."
-forever start /usr/src/eliot-services/eliot-mongo-timeseries-api/server.js
+    forever start /usr/src/eliot-services/eliot-mongo-timeseries-api/server.js
 echo "eliot-mongo-timeseries-api Started."
+echo "---------------------------------------------------------------------------"
 
+echo "Startup Logs"
+echo "---------------------------------------------------------------------------"
+    forever logs 0
+echo "---------------------------------------------------------------------------"
+    forever logs 1
+echo "---------------------------------------------------------------------------"
 echo "Done!"
