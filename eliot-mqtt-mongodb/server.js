15f9a1680338875b88767ff5e33320c78b0486e4
Migal Bothma <migal.bothma@bcx.co.za>
2019-08-20 14:47:34 +0200

    error finding

--- eliot-mqtt-mongodb/server.js
+++ eliot-mqtt-mongodb/server.js
@@ -3,6 +3,8 @@ var mqtt = require('mqtt');
 var config = require('./config');
 var moment = require('moment');
 
+var messageObject = {};
+
 //Setup MQTT
 var mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
 var client = mqtt.connect(mqttUri);
@@ -20,30 +22,28 @@ function setupCollection(err, database) {
 }
 
 function insertEvent(topic, message) {
-	if (topic && message) {
-		console.log("Time : " + moment().toISOString() + ", Topic : " + topic);
-		var messageObject = {};;
-		var _topicdata = topic.split("/");
+	console.log("Time : " + moment().toISOString() + ", Topic : " + topic);
+	
+	messageObject = {};
+	var _topicdata = topic.split("/");
+	var _company = _topicdata[0];
+	var _location = _topicdata[1];
+	var _area = _topicdata[2];
 
-		var _company = _topicdata[0];
-		var _location = _topicdata[1];
-		var _area = _topicdata[2];
+	messageObject = {
+		timestamp: moment().toISOString(),
+		company: _company,
+		location: _location,
+		area: _area,
+		data: JSON.parse(message)
+	};
 
-		messageObject = {
-			timestamp: moment().toISOString(),
-			company: _company,
-			location: _location,
-			area: _area,
-			data: JSON.parse(message)
-		};
-
-		//Insert object into DB
-		collection.insert(messageObject, function (error, result) {
-			if (error != null) {
-				console.log("ERROR: " + error);
-			}
-			console.log("MongoDB result : " + result);
-		});
-	}
+	//Insert object into DB
+	collection.insert(messageObject, function (error, result) {
+		if (error != null) {
+			console.log("ERROR: " + error);
+		}
+		console.log("MongoDB result : " + result);
+	});
 }
 
