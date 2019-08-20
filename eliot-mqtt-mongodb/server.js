2cc5a811bc8909663bfbb431fd776ef19694cd3b
Migal Bothma <migal.bothma@bcx.co.za>
2019-08-20 14:08:25 +0200

    mqtt server update

--- eliot-mqtt-mongodb/server.js
+++ eliot-mqtt-mongodb/server.js
@@ -3,55 +3,53 @@ var mqtt = require('mqtt');
 var config = require('./config');
 var moment = require('moment');
 
+//Setup MQTT
 var mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
 var client = mqtt.connect(mqttUri);
 
-client.on('connect', function () {
-	client.subscribe(config.mqtt.namespace);
-});
-
+//Setup and connect to MongoDB
 var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
-mongodb.MongoClient.connect(mongoUri, function (error, database) {
-	if (error != null) {
-		throw error;
+mongodb.MongoClient.connect(mongoUri, setupCollection);
+
+function setupCollection(err, database){
+	if(err) throw err;
+
+	collection = database.collection(config.mongodb.collection);
+	client.subscribe(config.mqtt.namespace);
+	client.on('message', insertEvent);
+}
+
+function insertEvent(){
+	console.log("Time : " + moment().toISOString() + ", Topic : " + topic);
+	var messageObject = {};
+	var _topicdata = topic.split("/");
+	
+	if(ParseInt(_topicdata.length) == 3){
+		var _company = _topicdata[0];
+		var _location = _topicdata[1];
+		var _area = _topicdata[2];
+	
+		messageObject = {
+			timestamp: moment().toISOString(),
+			company: _company,
+			location: _location,
+			area: _area,
+			data: JSON.parse(message)
+		};
+	}
+	else{
+		messageObject = {
+			timestamp: moment().toISOString(),
+			topic: topic,
+			message: message.toString(),
+			data: JSON.parse(message)
+		};
 	}
 
-	var collection = database.collection(config.mongodb.collection);
-	collection.createIndex({ "topic": 1 });
-
-	client.on('message', function (topic, message) {
-		console.log("Message : " + message + " from Topic : " + topic);
-		var messageObject = {};
-		var _topicdata = topic.split("/");
-
-		if (_topicdata.length == 3) {
-			var _company = _topicdata[0];
-			var _location = _topicdata[1];
-			var _area = _topicdata[2];
-
-			messageObject = {
-				timestamp: moment().toISOString(),
-				company: _company,
-				location: _location,
-				area: _area,
-				data: JSON.parse(message)
-			};
+	collection.insert(messageObject, function (error, result) {
+		if (error != null) {
+			console.log("ERROR: " + error);
 		}
-
-		else {
-			messageObject = {
-				timestamp: moment().toISOString(),
-				topic: topic,
-				message: message.toString(),
-				data: JSON.parse(message)
-			};
-		}
-
-		collection.insert(messageObject, function (error, result) {
-			if (error != null) {
-				console.log("ERROR: " + error);
-			}
-			console.log ("MongoDB result : " + result + '\n');
-		});
+		console.log ("MongoDB result : " + result);
 	});
-});
+}
