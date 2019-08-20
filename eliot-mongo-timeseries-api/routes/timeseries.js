bd80de6922d5720c57d7aa6d2451fa83cba64d5d
Migal Bothma <migal.bothma@bcx.co.za>
2019-08-20 18:56:47 +0200

    timeseries update

--- eliot-mongo-timeseries-api/routes/timeseries.js
+++ eliot-mongo-timeseries-api/routes/timeseries.js
@@ -5,13 +5,14 @@ const Event = require('../models/event');
 
 router.get('/', async (req, res, next) => {
     const result = {
-        "Functions" : {
-            "GET /timeseries/" : "Returns this functions list",
-            "GET /timeseries/all" : "Returns all timeseries entries (limited to 1440 events = 1 day)",
-            "GET /timeseries/company/:companyName" : "Returns timeseries data for a specific company",
-            "GET /timeseries/company/:company/location/:location" : "Returns all timeseries entries for Company + Location",
-            "GET /timeseries/company/:company/location/:location/area/:area" : "Returns all timeseries entries for Company + Location + Area",
-            "POST /timeseries/event" : "Post a timeseries entry to database/events (accepts JSON body)"
+        "Functions": {
+            "GET /timeseries/": "Returns this functions list",
+            "GET /timeseries/all": "Returns all entries (limited to 1440 events = 1 day)",
+            "GET /timeseries/company/:companyName": "Returns data for a specific company",
+            "GET /company/:company/timeseries": "Returns timeseries data for a specific company",
+            "GET /timeseries/company/:company/location/:location": "Returns all data entries for Company + Location",
+            "GET /timeseries/company/:company/location/:location/area/:area": "Returns all data entries for Company + Location + Area",
+            "POST /timeseries/event": "Post a data entry to database/events (accepts JSON body)"
         }
     };
     res.send(result);
@@ -35,7 +36,7 @@ router.get('/company/:company/location/:location', async (req, res, next) => {
     const _location = req.params.location;
     const result = await Event.find({
         company: _company,
-        location : _location
+        location: _location
     });
     res.send(result);
 });
@@ -46,8 +47,8 @@ router.get('/company/:company/location/:location/area/:area', async (req, res, n
     const _area = req.params.area;
     const result = await Event.find({
         company: _company,
-        location : _location,
-        area : _area
+        location: _location,
+        area: _area
     });
     res.send(result);
 });
@@ -59,42 +60,54 @@ router.get('/company/:company/timeseries', async (req, res, next) => {
     });
 
     var result = {};
-
     var locations = [];
     var areas = [];
-
     var uniqueLocations;
     var uniqueAreas;
+    var uniqueLocationData = {};
+    var uniqueAreaData = {};
 
-    //limit to 1440 results @ 1 min = 1 day
-    if(dbresult.length > 0)
-    {
-        if(dbresult.length > 1440){
+    //limit to 1440 results @ 1 min = 1 day p/device
+    if (dbresult.length > 0) {
+        if (dbresult.length > 1440) {
             dbresult = dbresult.slice(0, 1399);
         }
+
+        dbresult.forEach(event => {
+            locations.push(event["location"]);
+            areas.push(event["area"]);
+        });
+
+        //Get Distinct locations & areas
+        uniqueLocations = Array.from(new Set(locations));
+        uniqueAreas = Array.from(new Set(areas));
+
+        uniqueLocations.forEach(location => {
+            if (uniqueLocationData[location] == undefined) { uniqueLocationData[location] = []; }
+            dbresult.forEach(event => {
+                if (location == event["location"]) {
+                    uniqueAreas.forEach( area => {
+                        if( area == event["area"] && location == event["location"]){
+                            if (uniqueAreaData[area] == undefined) { uniqueAreaData[area] = []; }
+                            areaID = area;
+                            uniqueAreaData[area].push({ data: event["data"] })
+                        }
+                    });   
+                }      
+            })
+            uniqueLocationData[location].push( uniqueAreaData );   
+            uniqueAreaData = {};
+        });
+        res.send(uniqueLocationData);
     }
-
-    dbresult.forEach(event => {
-        locations.push(event["location"]);
-        areas.push(event["area"]);
-    });
-
-    //Get Distinct locations & areas
-    uniqueLocations = Array.from(new Set(locations));
-    uniqueAreas = Array.from(new Set(areas));
-
-    uniqueLocations.forEach( location => {
-        
-    });
-
-    console.log(uniqueLocations);
-
-    res.send(result);
+    else {
+        res.send.status(200, "No Data Found for company.")
+    }
 })
 
 router.post('/event', async (req, res, next) => {
     const event = new Event({
-        _id : new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.ObjectId(),
         timestamp: req.body.timestamp,
         company: req.body.company,
         location: req.body.location,
@@ -105,13 +118,13 @@ router.post('/event', async (req, res, next) => {
     });
     event
         .save()
-        .then( result => {
+        .then(result => {
             console.log(result);
         })
-        .catch( err => console.log(err));
+        .catch(err => console.log(err));
     res.status(201).json({
-        message : "Posting event to /timeseries",
-        eventCreated : event
+        message: "Posting event to /timeseries",
+        eventCreated: event
     })
 });
 
