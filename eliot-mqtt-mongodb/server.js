20ffa22f3fdaf0373add4a4fdd504b3bdc58ebb7
Migal Bothma <migal.bothma@bcx.co.za>
2019-09-14 16:12:03 +0200

    moment fix

--- eliot-mqtt-mongodb/server.js
+++ eliot-mqtt-mongodb/server.js
@@ -33,7 +33,7 @@ function insertEvent(topic, message) {
 	console.log(moment(message["timestamp"]).toISOString());
 
 	messageObject = {
-		timestamp: moment(message["timestamp"], moment.ISO_8601),
+		timestamp: moment(message["timestamp"], moment.ISO_8601).toISOString(),
 		inserted: moment().toISOString(),
 		company: _company,
 		location: _location,
